01fda47fa3479b7cfae4d097e88ccf80985124be
Harshwardhan A. Jagtap <jagtapharshwardhan2007@gmail.com>
2021-07-16 16:42:28 +0530

    square root and square added and footer removed

--- calculator.js
+++ calculator.js
@@ -1,60 +1,102 @@
 console.log('welcome to calculator');
 
-// common logic
-setInterval(() => {
-    pythagoreanTheorem();
-}, 1000);
-
-
-// standard calculator
+// Standard Calculator
 // resource for standard calculator
 let standardCalculator = document.getElementsByClassName('standardCalculator');
-let input = document.getElementById('input');
+let input = document.getElementById('input1');
 let button = document.querySelectorAll('button');
 let sum = '';
 
 // logic for standard calculator
-function standardClac() {
-    for (let item of button) {
-        item.addEventListener('click', (e) => {
-            buttonValue = e.target.innerText;
-            if (buttonValue == "X") {
-                buttonValue = "*";
-                sum += buttonValue;
-                input.value = sum;
-            } else if (buttonValue == "AC") {
+for (let item of button) {
+    item.addEventListener('click', (e) => {
+        buttonValue = e.target.innerText;
+        if (buttonValue == "X") {
+            buttonValue = "*";
+            sum += buttonValue;
+            input.value = sum;
+        } else if (buttonValue == "AC") {
+            sum = "";
+            input.value = sum;
+        } else if (buttonValue == "C") {
+            sum = "";
+            input.value = sum;
+        } else if (buttonValue == "=") {
+            input.value = eval(sum);
+            setTimeout(() => {
                 sum = "";
-                input.value = sum;
-            } else if (buttonValue == "C") {
-                sum = "";
-                input.value = sum;
-            } else if (buttonValue == "=") {
-                input.value = eval(sum);
-                setTimeout(() => {
-                    sum = "";
-                }, 1);
-            } else {
-                sum += buttonValue;
-                input.value = sum;
-            };
-        });
-    };
-}
+            }, 1);
+        } else {
+            sum += buttonValue;
+            input.value = sum;
+        };
+    });
+};
 
-
-// pythagorean theorem
+// Pythagorean Theorem
 function pythagoreanTheorem() {
     // resource for pythagorean theorem
     let input2p1 = document.getElementById("input2p1").value;
     let input2p2 = document.getElementById("input2p2").value;
 
     // logic for pythagorean theorem
-    let suboutputp1 = input2p1 * input2p1;
-    let suboutputp2 = input2p2 * input2p2;
-    let outcome2p1 = suboutputp1 + suboutputp2;
-    let result2 = Math.sqrt(outcome2p1);
+    let result2 = Math.hypot(input2p1, input2p2);
 
     // display pythagorean theorem
     let display2 = document.getElementById("2pT");
     display2.innerText = result2;
 }
+// repeating pythagorean theoream
+function repeatingPytha() {
+    setInterval(() => {
+        pythagoreanTheorem();
+    }, 100);
+}
+
+// Square Root
+function squareRoot(params) {
+    // resource for square root
+    let squareroot = document.querySelector("#input3");
+    let display3 = document.querySelector("#output3");
+
+    // logic for square root
+    let result3 = Math.sqrt(squareroot.value);
+    display3.innerText = result3;
+}
+// repeating square root
+function repeatingSquareRoot() {
+    setInterval(() => {
+        squareRoot();
+    }, 100);
+}
+
+// Square
+// resource for square
+function square() {
+    let input4 = document.getElementById("input4");
+    let result4 = Math.pow(input4.value, 2);
+    let output4 = document.getElementById("output4");
+    output4.innerText = result4;
+}
+
+function reapeatingSquare(params) {
+    setInterval(() => {
+        square();
+    }, 100);
+}
+
+
+// Selecting System (ss) 
+// resource for selecting system
+let select = document.querySelector(".select");
+
+// logic for selecting system
+select.addEventListener("change", function() {
+    if (select.value == "pythagoreanTheorem") {
+        repeatingPytha();
+    } else if (select.value == "squareroot") {
+        repeatingSquareRoot();
+    } else if (select.value == "square") {
+        reapeatingSquare();
+    }
+})
--- selecting system.js
+++ selecting system.js
@@ -1,9 +1,7 @@
 // when document is ready!!
 $(document).ready(function() {
-    // once we change selecting value
-    // this will happen
+    // once we change selecting value this will happen
     $(".select").on("change", function() {
-        // alert($(this).val());
         $(".dis").hide();
         $("#" + $(this).val()).fadeIn(500);
     }).change();
